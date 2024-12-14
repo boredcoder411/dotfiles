b29d1c6e51d413354b0c4e07b8ff9db127b77642
boredcoder411 <taamarcos@gmail.com>
2024-12-14 21:40:57 +0100

    added cursor color

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -9,5 +9,9 @@ local opacity = 0.8
 config.window_background_opacity = opacity
 
 config.enable_tab_bar = false
+config.colors = {
+    cursor_border = "#ffffff",
+    cursor_bg = "#ffffff",
+}
 
 return config
