d08e9e4e8bd543af22217a126c2bc830b00fc539
boredcoder411 <taamarcos@gmail.com>
2025-01-08 18:45:03 +0100

    added some more configs hehe

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -5,13 +5,15 @@ local config = wezterm.config_builder()
 --config.color_scheme = "Dracula"
 config.font = wezterm.font "DaddyTimeMono Nerd Font"
 
-local opacity = 0.8
-config.window_background_opacity = opacity
+config.window_background_opacity = 0.8
+config.macos_window_background_blur = 20
 
 config.enable_tab_bar = false
 config.colors = {
     cursor_border = "#ffffff",
     cursor_bg = "#ffffff",
 }
+
+config.window_close_confirmation = 'NeverPrompt'
 
 return config
